06f2b7c61f5e97201417903bccf4195cfec36dc9
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:22:17 -0600

    test: Add items to setup

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 RSpec.describe FoodTruck do
     before(:each) do
         @food_truck = FoodTruck.new("Rocky Mountain Pies")
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     describe "#initialize" do
