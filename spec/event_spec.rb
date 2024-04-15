b6ff0ebec79ea21015e020f4f91245727c8149c7
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:44:42 -0600

    test: Add setup/#initialize with readable attributes

---
+++ spec/event_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+RSpec.describe Event do
+    before(:each) do
+        @event = Event.new("South Pearl Street Farmers Market")  
+
+        @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+        @food_truck1.stock(@item1, 35)  
+        @food_truck1.stock(@item2, 7)  
+
+        @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom") 
+        @food_truck2.stock(@item4, 50)
+        @food_truck2.stock(@item3, 25)
+
+        @food_truck3 = FoodTruck.new("Palisade Peach Shack") 
+        @food_truck3.stock(@item1, 65)
+        
+        @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+        @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"}) 
+    end
+
+    describe "#initialize" do
+        it "exists" do
+            expect(@event).to be_an_instance_of(Event)
+        end
+
+        it "has a name" do
+            expect(@event.name).to eq("South Pearl Street Farmers Market")
+        end
+
+        it "has a list of food trucks" do
+            expect(@event.food_trucks).to eq([])
+        end
+    end
+end
