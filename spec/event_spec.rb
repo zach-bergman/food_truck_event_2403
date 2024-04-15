74d468a6d589abe4c8d33322f322ebc4bfaeecc8
Zach Bergman <bzach634@gmail.com>
2024-04-15 10:03:45 -0600

    refactor: Change #food_trucks_that_sell to take String as argument

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -64,8 +64,8 @@ RSpec.describe Event do
             @event.add_food_truck(@food_truck2)   
             @event.add_food_truck(@food_truck3)
 
-            expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
-            expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+            expect(@event.food_trucks_that_sell('Peach Pie (Slice)')).to eq([@food_truck1, @food_truck3])
+            expect(@event.food_trucks_that_sell("Banana Nice Cream")).to eq([@food_truck2])
         end
     end
 end
