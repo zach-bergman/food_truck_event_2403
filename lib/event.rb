82273766cc028b8fea596da0fb5d9f5f0324a5d5
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:52:15 -0600

    feat: Add #food_truck_names

--- lib/event.rb
+++ lib/event.rb
@@ -9,4 +9,8 @@ class Event
     def add_food_truck(food_truck)
         @food_trucks << food_truck
     end
+
+    def food_truck_names
+        @food_trucks.map { |food_truck| food_truck.name }
+    end
 end
