3039daed20c54aa4bb9a2d19e10c6c6a5b61c936
Zach Bergman <bzach634@gmail.com>
2024-04-15 10:15:48 -0600

    refactor: #food_trucks_that_sell to take item_name string as argument

--- lib/event.rb
+++ lib/event.rb
@@ -14,7 +14,11 @@ class Event
         @food_trucks.map { |food_truck| food_truck.name }
     end
 
-    def food_trucks_that_sell(item)
-        @food_trucks.select { |food_truck| food_truck.inventory.include?(item) }
+    def food_trucks_that_sell(item_name)
+        @food_trucks.select do |food_truck| 
+            food_truck.inventory.find do |item, amount|
+                item.name == item_name
+            end
+        end
     end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Event do
             @event.add_food_truck(@food_truck1)  
             @event.add_food_truck(@food_truck2)   
             @event.add_food_truck(@food_truck3)
-
+            
             expect(@event.food_trucks_that_sell('Peach Pie (Slice)')).to eq([@food_truck1, @food_truck3])
             expect(@event.food_trucks_that_sell("Banana Nice Cream")).to eq([@food_truck2])
         end
