61dbec675569b98e56110f08173843e28390c1cf
Zach Bergman <bzach634@gmail.com>
2024-04-15 10:24:30 -0600

    refactor: Change food_truck name for all other tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -14,41 +14,41 @@ RSpec.describe FoodTruck do
 
     describe "#initialize" do
         it "exists" do
-            expect(@food_truck).to be_an_instance_of(FoodTruck)
+            expect(@food_truck1).to be_an_instance_of(FoodTruck)
         end
 
         it "has a name" do
-            expect(@food_truck.name).to eq("Rocky Mountain Pies")
+            expect(@food_truck1.name).to eq("Rocky Mountain Pies")
         end
 
         it "has an inventory" do
-            expect(@food_truck.inventory).to eq({})
+            expect(@food_truck1.inventory).to eq({})
         end
     end
 
     describe "#check_stock" do
         it "returns the amount of a given item in the inventory" do
-            expect(@food_truck.check_stock(@item1)).to eq(0)
+            expect(@food_truck1.check_stock(@item1)).to eq(0)
         end
     end
 
     describe "#stock" do
         it "adds an item and amount to the inventory" do
-            expect(@food_truck.inventory).to eq({})
+            expect(@food_truck1.inventory).to eq({})
 
-            @food_truck.stock(@item1, 30)
+            @food_truck1.stock(@item1, 30)
 
-            expect(@food_truck.inventory).to eq({@item1 => 30})
+            expect(@food_truck1.inventory).to eq({@item1 => 30})
 
-            expect(@food_truck.check_stock(@item1)).to eq(30)
+            expect(@food_truck1.check_stock(@item1)).to eq(30)
 
-            @food_truck.stock(@item1, 25)
+            @food_truck1.stock(@item1, 25)
 
-            expect(@food_truck.check_stock(@item1)).to eq(55)
+            expect(@food_truck1.check_stock(@item1)).to eq(55)
 
-            @food_truck.stock(@item2, 12)
+            @food_truck1.stock(@item2, 12)
 
-            expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+            expect(@food_truck1.inventory).to eq({@item1 => 55, @item2 => 12})
         end
     end
 
