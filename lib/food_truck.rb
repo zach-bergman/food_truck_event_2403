759d41bd8277ee7c2af7f663ad0e2c640ba910c5
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:34:53 -0600

    feat: Add #stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -9,4 +9,8 @@ class FoodTruck
     def check_stock(item)
         @inventory[item]
     end
+
+    def stock(item, amount)
+        @inventory[item] += amount
+    end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe FoodTruck do
             expect(@food_truck.check_stock(@item1)).to eq(55)
 
             @food_truck.stock(@item2, 12)
-
+            
             expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
         end
     end
