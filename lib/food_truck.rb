284ff4362efb623136316ec7ac041eff3b68b764
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:28:11 -0600

    feat: Add #check_stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,12 @@
 class FoodTruck
     attr_reader :name, :inventory
-    
+
     def initialize(name)
         @name = name
-        @inventory = {}
+        @inventory = Hash.new(0)
+    end
+
+    def check_stock(item)
+        @inventory[item]
     end
 end
