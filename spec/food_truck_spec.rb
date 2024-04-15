68a21e2fac3c1c7fc4c33b5d034bf8a17aaf5402
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:32:58 -0600

    test: Add #stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -26,4 +26,24 @@ RSpec.describe FoodTruck do
             expect(@food_truck.check_stock(@item1)).to eq(0)
         end
     end
+
+    describe "#stock" do
+        it "adds an item and amount to the inventory" do
+            expect(@food_truck.inventory).to eq({})
+
+            @food_truck.stock(@item1, 30)
+
+            expect(@food_truck.inventory).to eq({@item1 => 30})
+
+            expect(@food_truck.check_stock(@item1)).to eq(30)
+
+            @food_truck.stock(@item1, 25)
+
+            expect(@food_truck.check_stock(@item1)).to eq(55)
+
+            @food_truck.stock(@item2, 12)
+
+            expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+        end
+    end
 end
