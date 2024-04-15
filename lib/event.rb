840547c7ff06f983df25a6174a9b4e4dc175cb07
Zach Bergman <bzach634@gmail.com>
2024-04-15 09:59:51 -0600

    feat: Add #food_trucks_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -13,4 +13,8 @@ class Event
     def food_truck_names
         @food_trucks.map { |food_truck| food_truck.name }
     end
+
+    def food_trucks_that_sell(item)
+        @food_trucks.select { |food_truck| food_truck.inventory.include?(item) }
+    end
 end
